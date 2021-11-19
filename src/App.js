520a9b93644d9daee4fd411caf98ae95f9faf062
Rahul <rahulmudumala2493@gmail.com>
2021-11-19 08:33:58 +0000

    solution

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import './App.css'
+
+import Users from './components/UsersDetails'
+
+const App = () => <Users />
+
+export default App
---
+++ src/components/Header/index.js
@@ -0,0 +1,34 @@
+import {
+  HeaderContainer,
+  NameHeading,
+  EmailHeading,
+  RoleHeading,
+  ActionHeading,
+  HeaderCheckboxInput,
+  HeaderContentContainer,
+} from './styledComponents'
+
+const Header = props => {
+  const {onChangeFunction, allChecked} = props
+
+  return (
+    <HeaderContainer>
+      <HeaderCheckboxInput
+        onChange={e => onChangeFunction(e)}
+        type="checkbox"
+        name="Header"
+        // checked={users.filter(user => user?.isChecked !== true).length < 1}
+        checked={allChecked}
+        value="Header0"
+      />
+      <HeaderContentContainer>
+        <NameHeading>Name</NameHeading>
+        <EmailHeading>Email</EmailHeading>
+        <RoleHeading>Role</RoleHeading>
+        <ActionHeading>Actions</ActionHeading>
+      </HeaderContentContainer>
+    </HeaderContainer>
+  )
+}
+
+export default Header
